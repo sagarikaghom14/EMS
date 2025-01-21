260179502c0f5e236aad37e4ea77aca0c45be603
sagarikaghom14 <sagarika.ghom@mitaoe.ac.in>
2025-01-21 23:52:16 +0530

    Admin Dashboard created

---
+++ src/components/other/AllTask.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div id='alltask' className='bg-[#1c1c1c] rounded p-5 h-60 overflow-auto mt-5'>
+        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Sagrika</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-green-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Sagrika</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-yellow-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Sagrika</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-blue-400 py-2 mb-2 px-4 flex justify-between rounded'>
+            <h2>Sagrika</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-purple-400 py-2 mb-2 px-4 flex justify-between rounded'>
+            <h2>Sagrika</h2>
+            <h3>Make a UI Design</h3>
+            <h5>Status</h5>
+        </div>
+      
+    </div>
+  )
+}
+
+export default AllTask
---
+++ src/components/other/CreateTask.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const CreateTask = () => {
+    return (
+        <div className='p-5 bg-[#1C1C1C] mt-5 rounded'>
+            <form className='flex flex-wrap w-full items-start justify-between'>
+                <div className='w-1/2'>
+                    <div>
+                        <h3 className='text-sm text-gray-300 mb-0.5'>Task Title</h3>
+                        <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Make a UI design' />
+                    </div>
+                    <div>
+                        <h3 className='text-sm text-gray-300 mb-0.5'>Date</h3>
+                        <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="date" />
+                    </div>
+                    <div>
+                        <h3 className='text-sm text-gray-300 mb-0.5'>Assign to</h3>
+                        <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='Employee Name' />
+                    </div>
+                    <div>
+                        <h3 className='text-sm text-gray-300 mb-0.5'>Category</h3>
+                        <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' type="text" placeholder='design, dev, etc' />
+                    </div>
+                </div>
+                <div className='w-2/5 flex flex-col items-start'>
+                    <h3 className='text-sm text-gray-300 mb-0.5'>Description</h3>
+                    <textarea className='text-sm py-2 px-4 w-full h-44 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4' name="" id="" cols="30" rows="10"></textarea>
+                    <button className='bg-emerald-500 py-3 px-5 rounded text-sm mt-4 w-full hover:bg-emerald-600'>Create Task</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default CreateTask
